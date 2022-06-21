e697f21d64ff0ec244fd66d9dfda4f4c3e5c79b8
Morgan Hamet <morganhamet@gmail.com>
2022-06-21 14:31:22 -0700

    Add basic to-do list functions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Taters");
+const todo = (title, dueDate, priority) => {
+    return { title, dueDate, priority };
+}
+
+const project = (description) => {
+    let todoList = [];
+    return { todoList, description };
+};
+
+const editProject = (() => {
+
+})();
+
+function addToProject(project, todo) {
+    project.todoList.push(todo);
+    return project.todoList;
+}
+
+function removeFromProject(project, todo) {
+    const index = project.todoList.indexOf(todo);
+    project.todoList.splice(index, 1);
+    return project.todoList;
+}
+
+const newProject = project("This is my project");
+const dishes = todo("Wash dishes", "10/22", "High");
+const cat = todo("Feed cat", "10/22", "Medium");
+console.log(addToProject(newProject, dishes));
